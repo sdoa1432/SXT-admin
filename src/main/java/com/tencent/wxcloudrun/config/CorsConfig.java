454c3fc5d8b8c44e969e6678c7bceb532cfa9bde
yc.zhang <344048127@qq.com>
2025-10-26 23:38:06 +0800

    跨域配置

--- src/main/java/com/tencent/wxcloudrun/config/CorsConfig.java
+++ src/main/java/com/tencent/wxcloudrun/config/CorsConfig.java
@@ -8,7 +8,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true)
                 .maxAge(3600)
